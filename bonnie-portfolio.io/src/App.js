7d6eb30d1bdd8c3ac50f132eb31fab051efd6785
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-20 14:59:48 +0300

    react portfolio

---
+++ bonnie-portfolio.io/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import HomeTabs from './components/HomeTabs';
+import Footer from './components/footer';
+
+
+function App() {
+  return (
+    <div>
+      <div className="app">
+        <header className="app-header">
+          <img src={logo}
+            className="app-logo" alt="logo"
+          />
+          <Header />
+        </header>
+        <HomeTabs />
+      </div>
+      <div className='icons-List'><Footer />
+      </div></div>
+  );
+}
+
+export default App;
